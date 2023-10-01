2ac24e1213f5a112f77151667fb04eae53ab0921
AJ <amarjitbhandal@gmail.com>
2023-10-01 20:21:36 +0400

    Ch-21 - Test, security tests.

--- ch-21-testing-resource-server/src/test/java/dev/aj/simple/controllers/DemoControllerTest.java
+++ ch-21-testing-resource-server/src/test/java/dev/aj/simple/controllers/DemoControllerTest.java
@@ -1,17 +1,16 @@
 package dev.aj.simple.controllers;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,14 +19,6 @@ class DemoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     // Have two different methodologies in this class, first one for OAuth2-Resource-Server, 2nd for 'http basic'
 
     @Test
@@ -35,26 +26,24 @@ class DemoControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/secure/{name}", "AJ")
 //                                              .with(jwt().authorities(new SimpleGrantedAuthority("admin")))
-                                              .with(SecurityMockMvcRequestPostProcessors.jwt().jwt(Jwt.withTokenValue("abc")
-                                                                                                .header("alg", "s32")
-                                                                                                .claim("user", "admin")
-                                                                                                .build())))
+                                              .with(jwt().jwt(Jwt.withTokenValue("abc")
+                                                                 .header("alg", "s32")
+                                                                 .claim("user", "admin")
+                                                                 .build())))
                .andExpect(MockMvcResultMatchers.status()
                                                .isOk())
                .andExpect(MockMvcResultMatchers.content()
                                                .string("Hello AJ"));
-
     }
 
     @Test
     @WithMockUser(authorities = "read")
     void postDemo() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/secure/{name}", "AJ")
-                                .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                                              .with(csrf()))
                .andExpect(MockMvcResultMatchers.status()
                                                .isOk())
                .andExpect(MockMvcResultMatchers.content()
                                                .string("Hello AJ"));
-
     }
 }
